116d3a4b58f9d90fe3283f560ffa54d51741052c
Adrian Marquez Bellido <234361644+xamarbel660@users.noreply.github.com>
2026-05-13 17:43:07 +0200

    Cambio minimo para que funcione la aplicacion

--- android/app/src/main/java/com/proponycome/app/MainActivity.java
+++ android/app/src/main/java/com/proponycome/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.proponycome.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getBridge().getWebView().getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+	}
+}
